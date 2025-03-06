fede243a8ffc68c5857055a618b94f330c2cf9aa
Miriam Schulz <mirischulz@gmail.com>
2025-03-06 17:37:10 +0100

    remove old code

--- stimuli/glove_stimuli.py
+++ stimuli/glove_stimuli.py
@@ -32,26 +32,11 @@ def get_glove_embeddings(df):
             df.loc[index, 'glove'] = sim
         except:
             df.loc[index, 'glove'] = 'NA'
-
-        # word1 = 'Kaffee'.lower()
-        # word2 = 'Bohne'.lower()
-        #
-        # # Check if words are in the embeddings
-        # if word1 in embeddings and word2 in embeddings:
-        #     vec1 = embeddings[word1]
-        #     vec2 = embeddings[word2]
-        #     similarity = cosine_similarity(vec1, vec2)
-        #     print(f"Cosine similarity between '{word1}' and '{word2}': {similarity}")
-        # else:
-        #     print(f"One or both words not found in the GloVe embeddings.")
     return(df)
-
-
 
 if __name__=='__main__':
 
     # Load GloVe embedding file
-    # embeddings = load_glove_embeddings('./glove.6B.300d.txt')
     embeddings = load_glove_embeddings('./multilingual_embeddings/multilingual_embeddings.de')
 
     # Optional: print vocab size
